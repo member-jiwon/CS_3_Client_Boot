0f343b794752d47c8c891afacdd8d9015ea6db6f
heybin <tlszn10@naver.com>
2025-11-24 10:17:26 +0900

    흠

--- src/main/java/com/kedu/project/chatbot/ChatBotController.java
+++ src/main/java/com/kedu/project/chatbot/ChatBotController.java
@@ -1,13 +1,23 @@
 package com.kedu.project.chatbot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RequestMapping("")
+
+@RequestMapping("/chatBoot")
 @RestController
 public class ChatBotController {
     @Autowired
     private ChatBotService chatBotService;    
+
+    // @GetMapping("/butList")
+    // public ResponseEntity<ChatBotDTO> butList() {
+    //     return ResponseEntity.ok(chatBotService.butList);
+    // }
+    
 }
