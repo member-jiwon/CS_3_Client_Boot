01ee081e76a56e5130cd9f4991350ed213466c13
Fuigang <quickly3899@naver.com>
2025-12-02 19:55:39 +0900

    1202 chart update not date

--- src/main/java/com/kedu/project/growth_chart/GrowthChartController.java
+++ src/main/java/com/kedu/project/growth_chart/GrowthChartController.java
@@ -10,9 +10,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -112,9 +112,8 @@ public class GrowthChartController {
 //        }
 //    }
 
-	 @PatchMapping("/chart/update/{babySeq}")
+	 @PutMapping("/update")
 	 public ResponseEntity<?> updateGrowthChart(
-	         @PathVariable int babySeq,
 	         @RequestBody List<GrowthChartDTO> updates
 	 ) {
 	     growthChartService.updateGrowthChart(updates);
