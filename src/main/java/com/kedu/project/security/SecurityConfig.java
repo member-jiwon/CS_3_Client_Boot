8c5cf7edf2af19d0100290533a0afe235d921642
heybin <tlszn10@naver.com>
2025-12-02 17:52:05 +0900

    집가기전 커밋

--- src/main/java/com/kedu/project/security/SecurityConfig.java
+++ src/main/java/com/kedu/project/security/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
 		config.addAllowedOrigin("http://192.168.0.6:3000");
 		config.addAllowedOrigin("http://10.10.55.80:3000");
         config.addAllowedOrigin("http://10.10.55.89:3000");
+        config.addAllowedOrigin("http://10.5.5.4:3002");
         config.addAllowedMethod("*");
 		config.addAllowedHeader("*");
         config.setAllowCredentials(true);
